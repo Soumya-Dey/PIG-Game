9482382c38a3a56a9184582ba1265a7708b55355
Soumya Dey <soumyadey200@hotmail.com>
2019-09-30 21:54:25 +0530

    added new features

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ GAME RULES:
 - The game has 2 players, playing in rounds
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- If the player rolls two 6 in a row, all his GLOBAL score gets lost
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
 var scores, roundScore, activePlayer, gameIsNotFinished;
-var diceDom = document.querySelector(".dice");
+var diceDom = document.getElementById("dice-1");
+var dice1Dom = document.getElementById("dice-2");
 var score0Dom = document.getElementById("score-0");
 var score1Dom = document.getElementById("score-1");
 var current0Dom = document.getElementById("current-0");
@@ -20,23 +22,25 @@ var name0Dom = document.getElementById("name-0");
 var name1Dom = document.getElementById("name-1");
 var btnRollDom = document.querySelector(".btn-roll");
 var btnHoldDom = document.querySelector(".btn-hold");
+var scoreInputDom = document.getElementById("final-score");
 
 init();
 
 // function for initializing a new game
-function init(){
+function init() {
     scores = [0, 0];
     roundScore = 0;
     activePlayer = 0; // for choosing the active player
     gameIsNotFinished = true;
 
     diceDom.style.display = "none";
+    dice1Dom.style.display = "none";
     score0Dom.textContent = "0";
     score1Dom.textContent = "0";
     current0Dom.textContent = "0";
     current1Dom.textContent = "0";
 
-	name0Dom.textContent = "Player 1";
+    name0Dom.textContent = "Player 1";
     name1Dom.textContent = "PLayer 2";
     panel0Dom.classList.remove("loser");
     panel1Dom.classList.remove("loser");
@@ -59,20 +63,24 @@ function togglePlayer() {
     panel0Dom.classList.toggle("active");
     panel1Dom.classList.toggle("active");
     diceDom.style.display = "none";
+    dice1Dom.style.display = "none";
 }
 
 document.querySelector(".btn-roll").addEventListener("click", function () {
-    if(gameIsNotFinished){
+    if (gameIsNotFinished) {
         // random number for dice rolling
         var dice = Math.floor(Math.random() * 6) + 1; // for getting a number between 1 and 6
+        var dice1 = Math.floor(Math.random() * 6) + 1;
 
         // showing the result score and dice
         diceDom.style.display = "block";
+        dice1Dom.style.display = "block";
         diceDom.src = "dice-" + dice + ".png";
+        dice1Dom.src = "dice-" + dice1 + ".png";
 
         // updating the round score if the rolled number is not 1
-        if (dice !== 1) {
-            roundScore += dice;
+        if (dice !== 1 && dice1 !== 1) {
+            roundScore += (dice + dice1);
             document.getElementById("current-" + activePlayer).textContent = roundScore;
         } else {
             togglePlayer();
@@ -81,16 +89,22 @@ document.querySelector(".btn-roll").addEventListener("click", function () {
 })
 
 document.querySelector(".btn-hold").addEventListener("click", function () {
-    if(gameIsNotFinished){
+    if (gameIsNotFinished) {
         // adding round score to the global score
         scores[activePlayer] += roundScore;
         document.getElementById("score-" + activePlayer).textContent = scores[activePlayer];
 
+        var winingScore;
+        var input = scoreInputDom.value;
+        if(input) winingScore = input;
+        else winingScore = 100;
+
         // checking if the active player has won
         var playerNameDom = document.getElementById("name-" + activePlayer);
-        if (scores[activePlayer] >= 100) {
+        if (scores[activePlayer] >= winingScore) {
             playerNameDom.textContent = "WINNER! PLAYER " + (activePlayer + 1);
             diceDom.style.display = "none";
+            dice1Dom.style.display = "none";
             document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
             document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
             document.querySelector(".player-" + (1 - activePlayer) + "-panel").classList.add("loser");
